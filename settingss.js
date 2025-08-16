35daf53aa3bbf44b9c2b9140cdc582acbb91be9e
ⵊͷຮᴛꫝ <evansken2008@gmail.com>
2025-08-16 01:20:40 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~hulQUCCL#l5N2l1Hq7tY4KmaTHWUWsYZessDxAWZp-6tVpINyhCU",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "212619417433", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "!", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
